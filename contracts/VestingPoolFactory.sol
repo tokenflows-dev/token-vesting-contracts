2e4892a5a27842bd7ffa1b658888cee21d210d55
XaNha <scorpion.f94@gmail.com>
2022-04-06 15:01:20 +0700

    Added VestingPoolFactory.sol

---
+++ contracts/VestingPoolFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Vesting.sol";
+
+contract VestingPoolFactory is Ownable {
+    using SafeMath for uint256;
+
+    address[] public vestingPools;
+
+    function createPool(
+        address _token,
+        uint256 _startTime,
+        uint256 _vestingDuration
+    ) external onlyOwner {
+        VestingPeriod pool = new VestingPeriod(
+            _token,
+            _startTime,
+            _vestingDuration
+        );
+
+        pool.transferOwnership(msg.sender);
+        vestingPools.push(address(pool));
+    }
+
+    function getPools() external view returns (address[] memory _pools) {
+        _pools = vestingPools;
+    }
+}
