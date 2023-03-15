e07c62ce42af9c586068b566416499f6e7ff88b9
Leon Acosta @ Dandelion Labs <leon@dandelionlabs.io>
2023-03-15 11:18:47 +0700

    added claim tests

--- test/linear-vesting/claims.ts
+++ test/linear-vesting/claims.ts
@@ -1,28 +1,49 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import {deployProjectFactory} from "./util/fixtures";
+import {deployProjectFactoryWithProjectAndPool} from "./util/fixtures";
+import {setNextBlockTimestamp} from "./util/time";
 
 describe("Claims", function () {
 
-  let projectFactory, erc20, owner, manager, user1, user2;
+  let poolVestingTime, startingPeriodTime, amount, projectFactory, erc20, project, owner, manager, user1, user2;
 
   beforeEach(async () => {
-    ({projectFactory, erc20, owner, manager, user1, user2 } = await loadFixture(deployProjectFactory));
+    ({startingPeriodTime, poolVestingTime, amount, projectFactory, erc20, project, owner, manager, user1, user2 } = await loadFixture(deployProjectFactoryWithProjectAndPool));
   });
 
   it("ongoing grant claim should succeed", async function () {
-    // todo check emitted event
+    const nextTs = startingPeriodTime + (poolVestingTime / 2); // elapsed half-time
+    await setNextBlockTimestamp(nextTs);
+
+    await expect(
+        project.connect(user1).claimVestedTokens(0)
+    ).to.emit(project, "GrantClaimed").withArgs(0, user1.address, amount.div(2))
+
+    expect(await project.claimedBalance(0, user1.address)).to.equal(amount.div(2));
+    expect(await project.vestedBalance(0, user1.address)).to.equal(amount.div(2));
   });
 
   it("invalid pool index should fail", async function () {
+    const nextTs = startingPeriodTime + (poolVestingTime / 2); // elapsed half-time
+    await setNextBlockTimestamp(nextTs);
 
+    await expect(
+        project.connect(user1).claimVestedTokens(1)
+    ).to.rejectedWith("LinearVestingProject::calculateGrantClaim: invalid pool index");
   });
 
-  it("invalid grant should fail", async function () {
+  it("invalid grantee should fail", async function () {
+    const nextTs = startingPeriodTime + (poolVestingTime / 2); // elapsed half-time
+    await setNextBlockTimestamp(nextTs);
 
+    await expect(
+        project.connect(user2).claimVestedTokens(0)
+    ).to.rejectedWith("VestingPeriod::claimVestedTokens: amountVested is 0'");
   });
 
   it("claimable balance 0 should fail", async function () {
-
+    await expect(
+        project.connect(user1).claimVestedTokens(0)
+    ).to.rejectedWith("VestingPeriod::claimVestedTokens: amountVested is 0'");
   });
 });
